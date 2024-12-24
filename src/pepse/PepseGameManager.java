77d9d4aa3736a4e26ca200736c9e065a8fcd5c0d
noamk <noam.kimhi@mail.huji.ac.il>
2024-12-24 18:19:23 +0200

    Phase 0

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -1,4 +1,31 @@
 package pepse;
 
-public class PepseGameManager {
+import danogl.GameManager;
+import danogl.GameObject;
+import danogl.collisions.Layer;
+import danogl.gui.ImageReader;
+import danogl.gui.SoundReader;
+import danogl.gui.UserInputListener;
+import danogl.gui.WindowController;
+import pepse.world.Sky;
+
+/** TODO: Docs */
+public class PepseGameManager extends GameManager {
+
+    /** TODO: Docs */
+    @Override
+    public void initializeGame(
+            ImageReader imageReader,
+            SoundReader soundReader,
+            UserInputListener inputListener,
+            WindowController windowController) {
+        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        GameObject sky = Sky.create(windowController.getWindowDimensions()); // create sky
+        gameObjects().addGameObject(sky, Layer.BACKGROUND); // add sky to background layer
+    }
+
+    /** TODO: Docs */
+    public static void main(String[] args) {
+        new PepseGameManager().run();
+    }
 }
