6debb2deeb0214f67fab48d77fe87dca03501528
or-forshmit8 <or.forshmit@mail.huji.ac.il>
2024-12-24 21:32:02 +0200

    Fixed sun cycle and added docstrings

--- src/pepse/world/daynight/Sun.java
+++ src/pepse/world/daynight/Sun.java
@@ -9,7 +9,9 @@ import danogl.util.Vector2;
 
 import java.awt.Color;
 
-/**TODO: Docs */
+/**
+ * This class is responsible for creating the sun game object.
+ */
 public class Sun {
 
     private static final Vector2 SUN_SIZE = Vector2.of(90, 90); // TODO: verify size
@@ -19,18 +21,32 @@ public class Sun {
     private static final float TWO_THIRDS_FACTOR = 2 / 3f;
     private static final float HALF_FACTOR = 0.5f;
 
+    /**
+     * Creates a sun game object.
+     * <p>
+     *     The sun is a yellow circle that moves in a circular path.
+     *     The sun moves in a circular path around the center of the screen.
+     *     The sun moves in a circular path with a given cycle length.
+     * </p>
+     * @param windowDimensions The dimensions of the game window.
+     * @param cycleLength The length of the sun cycle in seconds.
+     * @return The sun game object.
+     */
     public static GameObject create(Vector2 windowDimensions, float cycleLength) {
-        Vector2 initialSunCenter = windowDimensions.mult(HALF_FACTOR);
         Renderable sunRenderer = new OvalRenderable(Color.YELLOW);
         GameObject sun = new GameObject(Vector2.ZERO, SUN_SIZE, sunRenderer);
         sun.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
         sun.setTag(SUN_TAG);
-        Vector2 cycleCenter = Vector2.of(windowDimensions.x() * HALF_FACTOR, windowDimensions.y() * TWO_THIRDS_FACTOR);
 
+        Vector2 initialSunCenter = windowDimensions.mult(HALF_FACTOR);
+        Vector2 cycleCenter = Vector2.of(
+                windowDimensions.x() * HALF_FACTOR, windowDimensions.y() * TWO_THIRDS_FACTOR
+        );
 
         new Transition<>(
                 sun,
-                (Float angle) -> sun.setCenter(initialSunCenter.subtract(cycleCenter).rotated(angle).add(cycleCenter)),
+                (Float angle) ->
+                        sun.setCenter(initialSunCenter.subtract(cycleCenter).rotated(angle).add(cycleCenter)),
                 INITIAL_SUN_CYCLE_ANGLE,
                 FINAL_SUN_CYCLE_ANGLE,
                 Transition.LINEAR_INTERPOLATOR_FLOAT,
