35ed5a1844e8dab37a25465de9f3ffcd0c793478
or-forshmit8 <or.forshmit@mail.huji.ac.il>
2024-12-24 20:49:20 +0200

    Up until phase 3 - created cycles of day and night
    Added docstrings

--- src/pepse/world/Sky.java
+++ src/pepse/world/Sky.java
@@ -7,20 +7,33 @@ import danogl.util.Vector2;
 
 import java.awt.Color;
 
-/** TODO: Docs */
+/**
+ * This class is responsible for creating the sky GameObject.
+ * The sky is a GameObject that is a rectangle that covers the entire window.
+ *
+ * @author Noam Kimhi
+ * @author Or Forshmit
+ */
 public class Sky {
 
     private static final Color BASIC_SKY_COLOR = Color.decode("#80C6E5");
     private static final String SKY_TAG = "sky";
 
-    /** TODO: Docs */
+    /**
+     * Creates a sky GameObject.
+     * The sky is a GameObject that is a rectangle that covers the entire window.
+     * @param windowDimensions The dimensions of the window.
+     * @return The sky GameObject.
+     */
     public static GameObject create(Vector2 windowDimensions) {
         GameObject sky = new GameObject(
                 Vector2.ZERO, windowDimensions,
                 new RectangleRenderable(BASIC_SKY_COLOR)
         );
+
         sky.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
         sky.setTag(SKY_TAG);
+
         return sky;
     }
 
--- src/pepse/world/Terrain.java
+++ src/pepse/world/Terrain.java
@@ -9,7 +9,15 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-/** TODO: Docs */
+/**
+ * The Terrain class is responsible for generating the terrain of the game.
+ * The terrain is generated in blocks of a fixed size.
+ *
+ * @see Block
+ *
+ * @author Noam Kimhi
+ * @author Or Forshmit
+ */
 public class Terrain {
 
     private static final Color BASE_BACKGROUND_COLOR = new Color(212, 123, 74); // block color
@@ -18,39 +26,65 @@ public class Terrain {
     private static final String BLOCK_TAG = "ground";
     private static final int TERRAIN_DEPTH = 20;
 
-    /** TODO: Docs */
+    /**
+     * Constructor for the Terrain class.
+     * @param windowDimensions The dimensions of the window.
+     * @param seed The seed for the random number generator.
+     */
     public Terrain (Vector2 windowDimensions, int seed) {
         groundHeightAtX0 = windowDimensions.y() * TWO_THIRDS_FACTOR;
         // TODO: Use seed when getting to infinite world part
     }
 
-    /** TODO: Docs */
+    /**
+     * Returns the height of the ground at the given x position.
+     * @param x The x position.
+     * @return The height of the ground at the given x position.
+     */
     public float groundHeightAtX0(float x) {
         return groundHeightAtX0;
         // TODO: Think of something more complicated later on
     }
 
     /**
-     * TODO: Docs
+     * Creates a list of blocks in the given range.
+     * @param minX The minimum x position.
+     * @param maxX The maximum x position.
+     * @return A list of blocks in the given range.
      */
     public List<Block> createInRange(int minX, int maxX) {
         List<Block> blockList = new ArrayList<>();
+
         // Add blocks at increasing X positions to the list
         for (int x = getClosestMultToBlockSize(minX); x < getClosestMultToBlockSize(maxX); x += Block.SIZE) {
+
             float y = (float) Math.floor(groundHeightAtX0(x) / Block.SIZE) * Block.SIZE;
+
             for (int i = 0; i < TERRAIN_DEPTH; i++) {
-                // Create a rectangle with approximate color
+
                 Renderable rectangleBlock = new RectangleRenderable(
-                        ColorSupplier.approximateColor(BASE_BACKGROUND_COLOR));
+                        ColorSupplier.approximateColor(BASE_BACKGROUND_COLOR)
+                ); // Create a rectangle with approximate color
+
                 Block block = new Block(Vector2.of(x, y + i * Block.SIZE), rectangleBlock);
                 block.setTag(BLOCK_TAG); // set block tag to "ground"
-                blockList.add(block); // add to block list
+                blockList.add(block); // add to blockList
             }
         }
         return blockList;
     }
 
-    /** TODO: Docs */
+    /**
+     * Returns the closest (smaller) multiple of the block size to the given number.
+     * @param num The number.
+     * @return The closest (smaller) multiple of the block size to the given number.
+     *
+     * @see Block#SIZE
+     * @see Terrain#createInRange(int, int)
+     *
+     * @implNote This method is used to ensure that the blocks are created in multiples of the block size.
+     *           This is done to ensure that the blocks are aligned properly.
+     */
     private static int getClosestMultToBlockSize(int num) {
         return (int) Math.floor((double) num / Block.SIZE) * Block.SIZE;
     }
