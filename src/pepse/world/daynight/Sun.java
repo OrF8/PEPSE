39580fa4f84bbec08b4cea933ea4b3c7f5adefef
noamk <noam.kimhi@mail.huji.ac.il>
2024-12-24 21:22:51 +0200

    Fixed sun cycle

--- src/pepse/world/daynight/Sun.java
+++ src/pepse/world/daynight/Sun.java
@@ -16,19 +16,21 @@ public class Sun {
     private static final String SUN_TAG = "sun";
     private static final float INITIAL_SUN_CYCLE_ANGLE = 0;
     private static final float FINAL_SUN_CYCLE_ANGLE = 360;
+    private static final float TWO_THIRDS_FACTOR = 2 / 3f;
+    private static final float HALF_FACTOR = 0.5f;
 
     public static GameObject create(Vector2 windowDimensions, float cycleLength) {
-        Vector2 sunPosition = windowDimensions.mult(0.5f);
+        Vector2 initialSunCenter = windowDimensions.mult(HALF_FACTOR);
         Renderable sunRenderer = new OvalRenderable(Color.YELLOW);
-        GameObject sun = new GameObject(sunPosition, SUN_SIZE, sunRenderer);
+        GameObject sun = new GameObject(Vector2.ZERO, SUN_SIZE, sunRenderer);
         sun.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
         sun.setTag(SUN_TAG);
+        Vector2 cycleCenter = Vector2.of(windowDimensions.x() * HALF_FACTOR, windowDimensions.y() * TWO_THIRDS_FACTOR);
 
-        Vector2 cycleCenter = sunPosition.subtract(SUN_SIZE.mult(0.5f));
 
         new Transition<>(
                 sun,
-                (Float angle) -> sun.setCenter(sunPosition.subtract(cycleCenter).rotated(angle).add(cycleCenter)),
+                (Float angle) -> sun.setCenter(initialSunCenter.subtract(cycleCenter).rotated(angle).add(cycleCenter)),
                 INITIAL_SUN_CYCLE_ANGLE,
                 FINAL_SUN_CYCLE_ANGLE,
                 Transition.LINEAR_INTERPOLATOR_FLOAT,
