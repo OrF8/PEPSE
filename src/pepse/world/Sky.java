9f7491102108a95d0cd7019397f4fecf89a76a9c
noamk <noam.kimhi@mail.huji.ac.il>
2024-12-24 18:19:23 +0200

    Phase 0

--- src/pepse/world/Sky.java
+++ src/pepse/world/Sky.java
@@ -1,4 +1,27 @@
 package pepse.world;
 
+import danogl.GameObject;
+import danogl.components.CoordinateSpace;
+import danogl.gui.rendering.RectangleRenderable;
+import danogl.util.Vector2;
+
+import java.awt.Color;
+
+/** TODO: Docs */
 public class Sky {
+
+    private static final Color BASIC_SKY_COLOR = Color.decode("#80C6E5");
+    private static final String SKY_TAG = "sky";
+
+    /** TODO: Docs */
+    public static GameObject create(Vector2 windowDimensions) {
+        GameObject sky = new GameObject(
+                Vector2.ZERO, windowDimensions,
+                new RectangleRenderable(BASIC_SKY_COLOR)
+        );
+        sky.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
+        sky.setTag(SKY_TAG);
+        return sky;
+    }
+
 }
